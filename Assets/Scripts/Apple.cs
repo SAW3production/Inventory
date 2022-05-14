06e729421bd61daf1b7374123a8b31cdac7f6afa
SAW3production <105048323+SAW3production@users.noreply.github.com>
2022-05-14 13:09:34 +0300

    Apple item

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : IInventoryItem
+{
+    public bool isEquipeed { get ; set; }
+
+    public Type type => GetType();
+
+    public int maxItemsInInventorySlot { get; } 
+
+    public int amount { get ; set; }
+
+
+    public Apple(int maxItemsInInventorySlot)
+    {
+        this.maxItemsInInventorySlot = maxItemsInInventorySlot;
+    }
+    public IInventoryItem Clone()
+    {
+        return new Apple(maxItemsInInventorySlot)
+        {
+            amount = this.amount
+        }
+    }
+
+}
